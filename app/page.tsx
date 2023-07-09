efc23fa5afe890ce94d706c7c470c66c644ae69d
tmac.lzh <15156692746@163.com>
2023-07-09 19:12:45 +0800

    feat: create-next-app

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      app-index
+    </main>
+  );
+}
