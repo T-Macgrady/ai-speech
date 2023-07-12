471231650ccecf2eae3667c4e821f95b45982d96
tmac.lzh <15156692746@163.com>
2023-07-12 13:52:22 +0800

    fix: es

--- utils/Tts.ts
+++ utils/Tts.ts
@@ -41,136 +41,139 @@ type Synthesizer = HTMLAudioElement | SpeechSynthesis;
 type TTSEvent = SpeechSynthesisEvent | Event;
 
 class Tts extends EventTarget {
-  #target: Target;
-  #synthesizer: Synthesizer;
-  #dispatchBoundaries = true;
-  #fetchAudioData: FetchAudioData = async () => ({ audio: '', marks: [] });
-  #marks: PollySpeechMark[] = [];
-  #text = '';
-  #lang = '';
-  #aborter = new AbortController();
-  #initialized = false;
+  private target: Target;
+  private synthesizer: Synthesizer;
+  private dispatchBoundaries = true;
+  private fetchAudioData: FetchAudioData = async () => ({
+    audio: '',
+    marks: [],
+  });
+  private marks: PollySpeechMark[] = [];
+  private text = '';
+  private lang = '';
+  private aborter = new AbortController();
+  private initialized = false;
 
   constructor(options?: ControllerOptions) {
     super();
 
-    this.#lang = options?.lang ?? this.#lang;
-    this.#synthesizer = window.speechSynthesis;
-    this.#target = new SpeechSynthesisUtterance(this.#text);
-    this.#dispatchBoundaries =
-      options?.dispatchBoundaries ?? this.#dispatchBoundaries;
+    this.lang = options?.lang ?? this.lang;
+    this.synthesizer = window.speechSynthesis;
+    this.target = new SpeechSynthesisUtterance(this.text);
+    this.dispatchBoundaries =
+      options?.dispatchBoundaries ?? this.dispatchBoundaries;
 
     if (options?.fetchAudioData) {
-      this.#target = this.#synthesizer = new Audio();
-      this.#fetchAudioData = options.fetchAudioData;
+      this.target = this.synthesizer = new Audio();
+      this.fetchAudioData = options.fetchAudioData;
     } else {
-      this.#initWebSpeechVoice(options?.voice);
+      this.initWebSpeechVoice(options?.voice);
 
       if (window.speechSynthesis) {
         window.speechSynthesis.onvoiceschanged = () => {
-          this.#initWebSpeechVoice(options?.voice);
+          this.initWebSpeechVoice(options?.voice);
         };
       }
     }
   }
 
-  #initWebSpeechVoice(voice?: SpeechSynthesisVoice): void {
-    if (this.#target instanceof SpeechSynthesisUtterance) {
+  initWebSpeechVoice(voice?: SpeechSynthesisVoice): void {
+    if (this.target instanceof SpeechSynthesisUtterance) {
       let voices = window.speechSynthesis.getVoices();
 
       if (voice) {
-        this.#target.voice = voice;
+        this.target.voice = voice;
       }
 
-      if (this.#lang) {
-        voices = voices.filter((voice) => voice.lang === this.#lang);
-        this.#target.voice = voices[0] ?? null;
+      if (this.lang) {
+        voices = voices.filter((voice) => voice.lang === this.lang);
+        this.target.voice = voices[0] ?? null;
 
-        if (voice && voice.lang === this.#lang) {
-          this.#target.voice = voice;
+        if (voice && voice.lang === this.lang) {
+          this.target.voice = voice;
         }
       }
     }
   }
 
-  async #attachAudioSource(): Promise<void> {
-    if (this.#synthesizer instanceof HTMLAudioElement) {
+  async attachAudioSource(): Promise<void> {
+    if (this.synthesizer instanceof HTMLAudioElement) {
       let data: TTSAudioData | null = null;
 
       try {
-        data = await this.#fetchAudioData(this.#text);
+        data = await this.fetchAudioData(this.text);
       } catch (err) {
         if (err instanceof Error) {
-          this.#dispatchError(err.message);
+          this.dispatchError(err.message);
         }
       } finally {
         if (data?.audio) {
-          this.#synthesizer.src = data.audio;
-          this.#marks = data.marks ?? this.#marks;
+          this.synthesizer.src = data.audio;
+          this.marks = data.marks ?? this.marks;
         }
       }
     }
   }
 
-  #dispatchEnd(evt: TTSEvent): void {
+  dispatchEnd(evt: TTSEvent): void {
     this.dispatchEvent(new CustomEvent(Events.END, { detail: evt }));
   }
 
-  #dispatchError(msg?: string): void {
+  dispatchError(msg?: string): void {
     this.dispatchEvent(new CustomEvent(Events.ERROR, { detail: msg }));
   }
 
-  #dispatchReady(): void {
+  dispatchReady(): void {
     this.dispatchEvent(new Event(Events.READY));
   }
 
-  #dispatchPlaying(evt: TTSEvent): void {
+  dispatchPlaying(evt: TTSEvent): void {
     this.dispatchEvent(new CustomEvent(Events.PLAYING, { detail: evt }));
   }
 
-  #dispatchPaused(evt: TTSEvent): void {
+  dispatchPaused(evt: TTSEvent): void {
     this.dispatchEvent(new CustomEvent(Events.PAUSED, { detail: evt }));
   }
 
-  #dispatchBoundary(evt: TTSEvent, boundary: TTSBoundaryUpdate): void {
+  dispatchBoundary(evt: TTSEvent, boundary: TTSBoundaryUpdate): void {
     this.dispatchEvent(
       new CustomEvent(Events.BOUNDARY, { detail: { evt, boundary } }),
     );
   }
 
-  #dispatchVolume(volume: number): void {
+  dispatchVolume(volume: number): void {
     this.dispatchEvent(new CustomEvent(Events.VOLUME, { detail: volume }));
   }
 
-  #dispatchRate(rate: number): void {
+  dispatchRate(rate: number): void {
     this.dispatchEvent(new CustomEvent(Events.RATE, { detail: rate }));
   }
 
-  #dispatchPitch(pitch: number): void {
+  dispatchPitch(pitch: number): void {
     this.dispatchEvent(new CustomEvent(Events.PITCH, { detail: pitch }));
   }
 
-  async #playHtmlAudio(): Promise<void> {
-    const audio = this.#synthesizer as HTMLAudioElement;
+  async playHtmlAudio(): Promise<void> {
+    const audio = this.synthesizer as HTMLAudioElement;
 
     try {
       await audio.play();
     } catch (err) {
       if (err instanceof Error) {
-        this.#dispatchError(err.message);
+        this.dispatchError(err.message);
       }
     }
   }
 
-  #getPollySpeechMarkForAudioTime(time: number): PollySpeechMark {
-    const length = this.#marks.length;
-    let bestMatch = this.#marks[0];
+  getPollySpeechMarkForAudioTime(time: number): PollySpeechMark {
+    const length = this.marks.length;
+    let bestMatch = this.marks[0];
     let found = false;
     let i = 1;
 
     while (i < length && !found) {
-      if (this.#marks[i].time <= time) {
-        bestMatch = this.#marks[i];
+      if (this.marks[i].time <= time) {
+        bestMatch = this.marks[i];
       } else {
         found = true;
       }
@@ -184,107 +187,100 @@ class Tts extends EventTarget {
   /**
    * Not all browsers return `evt.charLength` on SpeechSynthesisUtterance `boundary` events.
    */
-  #getBoundaryWordCharLength(startIndex: number): number {
-    const match = this.#text.substring(startIndex).match(/.+?\b/);
+  getBoundaryWordCharLength(startIndex: number): number {
+    const match = this.text.substring(startIndex).match(/.+?\b/);
 
     return match ? match[0].length : 0;
   }
 
-  #clamp(value: number, min = 0, max = 1): number {
+  clamp(value: number, min = 0, max = 1): number {
     return Math.max(min, Math.min(value, max));
   }
 
   /**
    * Removes registered listeners and creates new abort controller.
    */
-  #recycle(): AbortSignal {
-    this.#aborter.abort();
-    this.#aborter = new AbortController();
+  recycle(): AbortSignal {
+    this.aborter.abort();
+    this.aborter = new AbortController();
 
-    return this.#aborter.signal;
+    return this.aborter.signal;
   }
 
-  #utteranceInit(): void {
-    if (this.#target instanceof SpeechSynthesisUtterance) {
-      const signal = this.#recycle();
+  utteranceInit(): void {
+    if (this.target instanceof SpeechSynthesisUtterance) {
+      const signal = this.recycle();
 
-      this.#target.addEventListener('end', this.#dispatchEnd.bind(this), {
+      this.target.addEventListener('end', this.dispatchEnd.bind(this), {
         signal,
       });
-      this.#target.addEventListener('start', this.#dispatchPlaying.bind(this), {
+      this.target.addEventListener('start', this.dispatchPlaying.bind(this), {
         signal,
       });
-      this.#target.addEventListener(
-        'resume',
-        this.#dispatchPlaying.bind(this),
-        {
-          signal,
-        },
-      );
-      this.#target.addEventListener('pause', this.#dispatchPaused.bind(this), {
+      this.target.addEventListener('resume', this.dispatchPlaying.bind(this), {
         signal,
       });
-      this.#target.addEventListener(
+      this.target.addEventListener('pause', this.dispatchPaused.bind(this), {
+        signal,
+      });
+      this.target.addEventListener(
         'error',
         (evt) => {
-          this.#dispatchError(evt.error);
+          this.dispatchError(evt.error);
         },
         { signal },
       );
 
-      if (this.#lang) {
-        this.#target.lang = this.#lang;
+      if (this.lang) {
+        this.target.lang = this.lang;
       }
 
-      if (this.#dispatchBoundaries) {
-        this.#target.addEventListener(
+      if (this.dispatchBoundaries) {
+        this.target.addEventListener(
           'boundary',
           (evt) => {
             const { charIndex: startChar } = evt;
             const charLength =
-              evt.charLength ?? this.#getBoundaryWordCharLength(startChar);
+              evt.charLength ?? this.getBoundaryWordCharLength(startChar);
             const endChar = startChar + charLength;
-            const word = this.#text.substring(startChar, endChar);
+            const word = this.text.substring(startChar, endChar);
 
             if (word && !isPunctuation(word)) {
-              this.#dispatchBoundary(evt, { word, startChar, endChar });
+              this.dispatchBoundary(evt, { word, startChar, endChar });
             }
           },
           { signal },
         );
       }
 
-      this.#dispatchReady();
+      this.dispatchReady();
     }
   }
 
-  async #htmlAudioInit(): Promise<void> {
-    if (this.#target instanceof HTMLAudioElement) {
-      const target = this.#target;
+  async htmlAudioInit(): Promise<void> {
+    if (this.target instanceof HTMLAudioElement) {
+      const target = this.target;
 
-      this.#target.addEventListener('canplay', this.#dispatchReady.bind(this), {
+      this.target.addEventListener('canplay', this.dispatchReady.bind(this), {
         once: true,
       });
-      this.#target.addEventListener(
-        'playing',
-        this.#dispatchPlaying.bind(this),
-      );
-      this.#target.addEventListener('pause', this.#dispatchPaused.bind(this));
-      this.#target.addEventListener('ended', this.#dispatchEnd.bind(this));
-      this.#target.addEventListener('error', () => {
+      this.target.addEventListener('playing', this.dispatchPlaying.bind(this));
+      this.target.addEventListener('pause', this.dispatchPaused.bind(this));
+      this.target.addEventListener('ended', this.dispatchEnd.bind(this));
+      this.target.addEventListener('error', () => {
         const error = target.error;
 
-        this.#dispatchError(error?.message);
+        this.dispatchError(error?.message);
       });
 
-      if (this.#dispatchBoundaries) {
-        this.#target.addEventListener('timeupdate', (evt) => {
+      if (this.dispatchBoundaries) {
+        this.target.addEventListener('timeupdate', (evt) => {
           // Polly Speech Marks use milliseconds
           const currentTime = target.currentTime * 1000;
-          const mark = this.#getPollySpeechMarkForAudioTime(currentTime);
+          const mark = this.getPollySpeechMarkForAudioTime(currentTime);
 
           if (mark && !this.paused) {
-            this.#dispatchBoundary(evt, {
+            this.dispatchBoundary(evt, {
               word: mark.value,
               startChar: mark.start,
               endChar: mark.end,
@@ -293,58 +289,58 @@ class Tts extends EventTarget {
         });
       }
 
-      await this.#attachAudioSource();
+      await this.attachAudioSource();
     }
   }
 
   get synthesizer(): Synthesizer {
-    return this.#synthesizer;
+    return this.synthesizer;
   }
 
   get target(): Target {
-    return this.#target;
+    return this.target;
   }
 
   set text(value: string) {
-    this.#text = value;
+    this.text = value;
 
-    if (this.#target instanceof SpeechSynthesisUtterance) {
-      this.#target.text = value;
+    if (this.target instanceof SpeechSynthesisUtterance) {
+      this.target.text = value;
     }
   }
 
   get paused(): boolean {
-    return this.#synthesizer.paused;
+    return this.synthesizer.paused;
   }
 
   get rate(): number {
-    if (this.#synthesizer instanceof HTMLAudioElement) {
-      return this.#synthesizer.playbackRate;
+    if (this.synthesizer instanceof HTMLAudioElement) {
+      return this.synthesizer.playbackRate;
     }
 
-    return (this.#target as SpeechSynthesisUtterance).rate;
+    return (this.target as SpeechSynthesisUtterance).rate;
   }
 
   set rate(value: number) {
-    const clamped = this.#clamp(parseFloat(value.toPrecision(3)), 0.1, 10);
+    const clamped = this.clamp(parseFloat(value.toPrecision(3)), 0.1, 10);
 
     if (!Number.isNaN(clamped)) {
-      this.#dispatchRate(clamped);
+      this.dispatchRate(clamped);
 
-      if (this.#synthesizer instanceof HTMLAudioElement) {
-        this.#synthesizer.defaultPlaybackRate = clamped;
-        this.#synthesizer.playbackRate = clamped;
+      if (this.synthesizer instanceof HTMLAudioElement) {
+        this.synthesizer.defaultPlaybackRate = clamped;
+        this.synthesizer.playbackRate = clamped;
       }
 
-      if (this.#target instanceof SpeechSynthesisUtterance) {
-        this.#target.rate = clamped;
+      if (this.target instanceof SpeechSynthesisUtterance) {
+        this.target.rate = clamped;
       }
     }
   }
 
   get pitch(): number {
-    if (this.#target instanceof SpeechSynthesisUtterance) {
-      return this.#target.pitch;
+    if (this.target instanceof SpeechSynthesisUtterance) {
+      return this.target.pitch;
     }
 
     // Not supported by HTMLAudioElement
@@ -352,12 +348,12 @@ class Tts extends EventTarget {
   }
 
   set pitch(value: number) {
-    if (this.#target instanceof SpeechSynthesisUtterance) {
-      const clamped = this.#clamp(parseFloat(value.toPrecision(2)), 0, 2);
+    if (this.target instanceof SpeechSynthesisUtterance) {
+      const clamped = this.clamp(parseFloat(value.toPrecision(2)), 0, 2);
 
       if (!Number.isNaN(clamped)) {
-        this.#dispatchPitch(clamped);
-        this.#target.pitch = clamped;
+        this.dispatchPitch(clamped);
+        this.target.pitch = clamped;
       }
     }
   }
@@ -371,26 +367,26 @@ class Tts extends EventTarget {
   }
 
   get volume(): number {
-    return this.#target.volume;
+    return this.target.volume;
   }
 
   set volume(value: number) {
-    const clamped = this.#clamp(
+    const clamped = this.clamp(
       parseFloat(value.toPrecision(2)),
       this.volumeMin,
       this.volumeMax,
     );
 
     if (!Number.isNaN(clamped)) {
-      this.#dispatchVolume(clamped);
-      this.#target.volume = clamped;
+      this.dispatchVolume(clamped);
+      this.target.volume = clamped;
     }
   }
 
   get preservesPitch(): boolean {
-    if (this.#synthesizer instanceof HTMLAudioElement) {
+    if (this.synthesizer instanceof HTMLAudioElement) {
       return (
-        this.#synthesizer as HTMLAudioElement & { preservesPitch: boolean }
+        this.synthesizer as HTMLAudioElement & { preservesPitch: boolean }
       ).preservesPitch;
     }
 
@@ -402,24 +398,24 @@ class Tts extends EventTarget {
      * `preservesPitch` requires vendor-prefix on some browsers (Safari).
      * @see https://github.com/microsoft/TypeScript-DOM-lib-generator/pull/1300
      */
-    if (this.#synthesizer instanceof HTMLAudioElement) {
+    if (this.synthesizer instanceof HTMLAudioElement) {
       // eslint-disable-next-line @typescript-eslint/no-extra-semi
       (
-        this.#synthesizer as HTMLAudioElement & { preservesPitch: boolean }
+        this.synthesizer as HTMLAudioElement & { preservesPitch: boolean }
       ).preservesPitch = value;
     }
   }
 
   get lang(): string {
-    return this.#lang;
+    return this.lang;
   }
 
   set lang(value: string) {
-    if (this.#target instanceof SpeechSynthesisUtterance) {
-      this.#lang = value;
-      this.#target.lang = value;
-      this.#target.voice = null;
-      this.#initWebSpeechVoice();
+    if (this.target instanceof SpeechSynthesisUtterance) {
+      this.lang = value;
+      this.target.lang = value;
+      this.target.voice = null;
+      this.initWebSpeechVoice();
     }
   }
 
@@ -431,58 +427,58 @@ class Tts extends EventTarget {
    * Run it as async to allow for the fetchAudioData call to be awaited.
    */
   async init(): Promise<void> {
-    if (!this.#initialized) {
-      if (this.#target instanceof SpeechSynthesisUtterance) {
-        this.#utteranceInit();
+    if (!this.initialized) {
+      if (this.target instanceof SpeechSynthesisUtterance) {
+        this.utteranceInit();
       }
 
-      if (this.#target instanceof HTMLAudioElement) {
-        await this.#htmlAudioInit();
+      if (this.target instanceof HTMLAudioElement) {
+        await this.htmlAudioInit();
       }
 
-      this.#initialized = true;
+      this.initialized = true;
     }
   }
 
   async play(): Promise<void> {
-    if (this.#synthesizer instanceof HTMLAudioElement) {
-      await this.#playHtmlAudio();
+    if (this.synthesizer instanceof HTMLAudioElement) {
+      await this.playHtmlAudio();
     } else {
-      await this.#synthesizer.speak(this.#target as SpeechSynthesisUtterance);
+      await this.synthesizer.speak(this.target as SpeechSynthesisUtterance);
     }
   }
 
   pause(): void {
-    this.#synthesizer.pause();
+    this.synthesizer.pause();
   }
 
   async resume(): Promise<void> {
-    if (this.#synthesizer instanceof HTMLAudioElement) {
-      await this.#playHtmlAudio();
+    if (this.synthesizer instanceof HTMLAudioElement) {
+      await this.playHtmlAudio();
     } else {
-      this.#synthesizer.resume();
+      this.synthesizer.resume();
     }
   }
 
   async replay(): Promise<void> {
-    if (this.#synthesizer instanceof HTMLAudioElement) {
-      this.#synthesizer.load();
-      await this.#playHtmlAudio();
+    if (this.synthesizer instanceof HTMLAudioElement) {
+      this.synthesizer.load();
+      await this.playHtmlAudio();
     } else {
       // Take out of any paused state
-      this.#synthesizer.resume();
+      this.synthesizer.resume();
       // Drop all utterances in the queue
-      this.#synthesizer.cancel();
+      this.synthesizer.cancel();
       // Start speaking from the beginning
-      this.#synthesizer.speak(this.#target as SpeechSynthesisUtterance);
+      this.synthesizer.speak(this.target as SpeechSynthesisUtterance);
     }
   }
 
   cancel(): void {
-    if (this.#synthesizer instanceof HTMLAudioElement) {
-      this.#synthesizer.load();
+    if (this.synthesizer instanceof HTMLAudioElement) {
+      this.synthesizer.load();
     } else {
-      this.#synthesizer.cancel();
+      this.synthesizer.cancel();
     }
   }
 
@@ -494,9 +490,9 @@ class Tts extends EventTarget {
      * If there is currently an utterance being spoken, replay to activate the muting instantly.
      */
     if (
-      !(this.#synthesizer instanceof HTMLAudioElement) &&
+      !(this.synthesizer instanceof HTMLAudioElement) &&
       !this.paused &&
-      this.#synthesizer.speaking
+      this.synthesizer.speaking
     ) {
       this.replay();
     }
@@ -509,20 +505,20 @@ class Tts extends EventTarget {
      * Same as muting, for SpeechSynthesis have to replay to activate the volume change instantly.
      */
     if (
-      !(this.#synthesizer instanceof HTMLAudioElement) &&
+      !(this.synthesizer instanceof HTMLAudioElement) &&
       !this.paused &&
-      this.#synthesizer.speaking
+      this.synthesizer.speaking
     ) {
       this.replay();
     }
   }
 }
 
-export { Tts, Events };
+export { Events, Tts };
 export type {
-  TTSAudioData,
-  PollySpeechMark,
   ControllerOptions,
+  PollySpeechMark,
+  TTSAudioData,
   TTSBoundaryUpdate,
   TTSEvent,
 };
