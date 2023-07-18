ebc10fe190301b53729d5493f8d9ce7a46cf1cb8
tmac.lzh <15156692746@163.com>
2023-07-18 12:18:27 +0800

    feat: update chat

--- components/RealTimeChat.tsx
+++ components/RealTimeChat.tsx
@@ -4,7 +4,6 @@ import { useMessageLog } from '@/hooks/useMessageLog';
 import useSpeech2Text from '@/hooks/useSpeech2Text';
 import useText2Speech from '@/hooks/useText2Speech';
 import { extractLangAndClean, getLang } from '@/utils/lang';
-import { useWhisper } from '@tmacc/use-speech-to-text';
 import { useCallback, useEffect, useState } from 'react';
 
 export default function RealTimeChat() {
@@ -38,7 +37,6 @@ export default function RealTimeChat() {
   );
 
   useEffect(() => {
-    // console.log('--useEffect--', clappr, clappr?.Player);
     systemSay(
       '1.你作为一个人工智能助手，解决用户的各种问题；2.用户使用哪种语言提问你就使用对应语言回答，除非用户让你使用特定语言回答；3.每次响应最前面返回当前语言对应的 BCP 47 语言标签规范的lang，比如zh-CN、en-US，用<lang>标签包裹lang，之后是响应内容，示例：<lang>zh-CN</lang>中文响应',
     );
@@ -59,56 +57,13 @@ export default function RealTimeChat() {
 
       startSpeech2text();
     },
-    // onEnd(text, e) {
-    //   e?.error === 'no-speech' ? startSpeech2text() : stopSpeech2text();
-    // },
   });
 
-  const {
-    recording,
-    speaking,
-    transcribing,
-    transcript,
-    pauseRecording,
-    startRecording,
-    stopRecording,
-  } = useWhisper({
-    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_TOKEN,
-    streaming: false,
-    autoStart: false,
-    removeSilence: false,
-    nonStop: true, // keep recording as long as the user is speaking
-    stopTimeout: 2000, // auto stop after 2 seconds
-    timeSlice: 1000, // 1 second
-    whisperConfig: {
-      language: '',
-    },
-    onSpeakingCallback: (isSpeaking: boolean) => {
-      if (!isSpeaking) return;
-
-      if (!recording && !speaking && !transcribing) {
-        startRecording();
-        console.log('--startRecording--');
-      }
-    },
-    onWhisperedCallback: async (text: string) => {
-      if (
-        !text ||
-        ['Thanks for watching!', 'Thank you for watching!', 'You'].includes(
-          text,
-        )
-      )
-        return;
-
-      await getCompletionAndSpeak(text);
-    },
-  });
   return (
     <>
       <div className={`fixed inset-0 z-10 p-4 overflow-y-auto`}>
         <h2 className="font-bold mb-1">语音识别</h2>
         <p>isTranscribing: {String(isTranscribing)}</p>
-        {/* <p>isSpeaking: {String(isSpeaking)}</p> */}
         <p className="text-blue-500">{recognizeText}</p>
         <h2 className="font-bold mb-1">chatgpt补全</h2>
         <p className="text-blue-500">{completion?.content}</p>
@@ -150,33 +105,6 @@ export default function RealTimeChat() {
             </li>
           ))}
         </ul>
-
-        {/* <h2 className="font-bold mb-4">chatgpt语音识别</h2>
-
-        <p>Recording: {String(recording)}</p>
-        <p>Transcribing: {String(transcribing)}</p>
-        <p>Speaking: {String(speaking)}</p>
-        <p>Transcribed Text: {transcript.text}</p>
-        <div className="flex space-x-4">
-          <button
-            className="mb-5 bg-blue-500 text-white py-2 px-4 rounded"
-            onClick={() => startRecording()}
-          >
-            start
-          </button>
-          <button
-            className="mb-5 bg-blue-500 text-white py-2 px-4 rounded"
-            onClick={() => pauseRecording()}
-          >
-            Pause
-          </button>
-          <button
-            className="mb-5 bg-blue-500 text-white py-2 px-4 rounded"
-            onClick={() => stopRecording()}
-          >
-            Stop
-          </button>
-        </div> */}
       </div>
     </>
   );
