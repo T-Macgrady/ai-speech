abfd71d1e2d93026b93ab2b86174ed309cbcfdbc
tmac.lzh <15156692746@163.com>
2023-07-19 00:26:51 +0800

    feat: translate

--- app/SimultaneousInterpretation/page.tsx
+++ app/SimultaneousInterpretation/page.tsx
@@ -1,8 +1,12 @@
 'use client';
+import CurrentConversation from '@/components/CurrentConversation';
+import MessageLog from '@/components/MessageLog';
+import RecordButton from '@/components/RecordButton';
 import { useChatCompletion } from '@/hooks/useChatCompletion';
 import { useMessageLog } from '@/hooks/useMessageLog';
 import useSpeech2Text from '@/hooks/useSpeech2Text';
-import { useCallback, useEffect, useState } from 'react';
+import classNames from 'classnames';
+import { useCallback, useState } from 'react';
 import { FaExchangeAlt } from 'react-icons/fa';
 
 const fillPromptENtoCN = (prompt) =>
@@ -14,13 +18,7 @@ const fillPromptCNtoEN = (prompt) =>
 const SimultaneousInterpretation = () => {
   const [lang, setLang] = useState('en-CN');
   const { messageLog, userSay, assistantSay, reset } = useMessageLog();
-  const { getCompletionStream } = useChatCompletion();
-
-  useEffect(() => {
-    const welcomeMessage =
-      'Hello, I am your assistant. Please speak, and I will translate.';
-    getCompletionAndSay(welcomeMessage);
-  }, []);
+  const { isFetching, getCompletionStream } = useChatCompletion();
 
   const fillPrompt = lang === 'en-CN' ? fillPromptENtoCN : fillPromptCNtoEN;
   const getCompletionAndSay = useCallback(
@@ -55,69 +53,60 @@ const SimultaneousInterpretation = () => {
     prompt: currentText ? fillPrompt(currentText) : '',
   });
 
-  //原先的代码部分未变，以下仅显示修改的部分
-
   return (
-    <div className="h-screen bg-gray-50 flex flex-col justify-between transition-all duration-300 p-4 ease-in-out space-y-4">
-      <div className="flex justify-between items-center">
-        <div className="text-green-500 text-xl font-bold">实时转译</div>
-        <button
-          onClick={toggleLang}
-          className="bg-green-200 hover:bg-green-300  text-sm text-gray-800 font-bold p-2 rounded transition-all duration-300 ease-in-out transform hover:scale-105 shadow-md flex items-center"
-        >
-          <FaExchangeAlt className="mr-2" />
-          {lang === 'en-CN' ? '英译中' : '中译英'}
-        </button>
-      </div>
-      <section className="mb-4 p-4 max-w-lg w-full bg-white rounded shadow-lg overflow-y-auto transition-all duration-300 ease-in-out transform hover:scale-105">
-        <div className="mt-2 grid grid-cols-2 gap-2">
-          <div>
-            <h2 className="text-sm font-bold text-gray-900">识别文本</h2>
-            {currentText ? (
-              <div className="text-blue-500">{currentText}</div>
-            ) : (
-              <div className="text-gray-500">暂无</div>
-            )}
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 h-screen pt-2">
+      <div
+        className={`flex flex-col h-full ${
+          messageLog.length > 1 ? 'md:col-span-2' : 'md:col-span-3'
+        }`}
+      >
+        <div className="flex-grow flex flex-col justify-center items-center">
+          <div className="mb-4">
+            <h1 className="text-4xl font-bold text-center">ChatGPT 中英同传</h1>
+            <p className="text-gray-500 text-center mt-2">
+              语音输入，AI 翻译，让同传更自然
+            </p>
           </div>
-          <div>
-            <h2 className="text-sm font-bold text-gray-900">转译结果</h2>
-            {currentCompletion?.content ? (
-              <div className="text-blue-500">{currentCompletion.content}</div>
-            ) : (
-              <div className="text-gray-500">暂无</div>
-            )}
-          </div>
-        </div>
-      </section>
-      <section className="flex flex-1 flex-col bg-white rounded shadow-lg overflow-y-auto mx-auto my-4 p-4 pt-0 max-w-lg w-full transition-all duration-300 ease-in-out transform hover:scale-105">
-        <div className="sticky top-0 bg-white py-2 z-10">
-          <div className="text-green-500 text-xl font-bold">转译历史</div>
-        </div>
-        {[...messageLog].reverse().map((message, index) => (
-          <div
-            key={index}
-            className={`py-2 px-4 my-2 rounded border-l-4 ${
-              message.role === 'user'
-                ? 'border-blue-500 bg-blue-100'
-                : 'border-green-500 bg-green-100'
-            }`}
+
+          {/* toggle */}
+          <button
+            onClick={toggleLang}
+            className="mb-4 bg-green-200 hover:bg-green-300  text-sm text-gray-800 font-bold p-2 rounded transition-all duration-300 ease-in-out transform hover:scale-105 shadow-md flex items-center"
           >
-            {message.content}
+            <FaExchangeAlt className="mr-2" />
+            {lang === 'en-CN' ? '英译中' : '中译英'}
+          </button>
+
+          {/* Button */}
+          <div
+            className={classNames(
+              'mb-4 flex-grow flex flex-col justify-center items-center transition-all duration-300 ease-in-out',
+              {
+                'scale-150 translate-y-16':
+                  !currentText && messageLog.length <= 1,
+              },
+            )}
+          >
+            <RecordButton
+              isTranscribing={isTranscribing}
+              // isSpeeching={isSpeeching}
+              isFetching={isFetching}
+              onStart={startSpeech2Text}
+              onStop={stopSpeech2Text}
+            />
           </div>
-        ))}
-      </section>
-      <div className="flex justify-center items-center space-x-4">
-        <button
-          onClick={isTranscribing ? stopSpeech2Text : startSpeech2Text}
-          className={`w-48 h-10 py-2 rounded transition-all duration-300 ease-in-out transform hover:scale-110 text-white font-bold ${
-            isTranscribing
-              ? 'bg-red-500 hover:bg-red-700'
-              : 'bg-blue-500 hover:bg-blue-700'
-          }`}
-        >
-          {isTranscribing ? '停止' : '开始'}
-        </button>
+
+          {currentText && (
+            <CurrentConversation
+              currentCompletion={currentCompletion}
+              recognizeText={currentText}
+            />
+          )}
+        </div>
       </div>
+
+      {/* history */}
+      <MessageLog messageLog={messageLog} />
     </div>
   );
 };
